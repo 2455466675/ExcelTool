f53352365399f1ce96f0f6334809fc549e70a794
lichuang <lichuang@xcarry.com>
2024-11-28 11:11:32 +0800

    生成的核心类和模型类分开

--- ExcelTool/CfgModel.cs
+++ ExcelTool/CfgModel.cs
@@ -3,6 +3,7 @@
     internal class CfgModel
     {
         public string excelPath;
+        public string coreFileOutputPaht;
         public string csFileOutputPaht;
         public string cfgFileOutputPaht;
         public string language;
